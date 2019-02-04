a93d3bb080a6e95dd47287436d1b54cf002cfa73
kvasnicaj <j.kvasnica@gmail.com>
2019-02-04 15:46:53 +0100

    search index bux fix

--- frontend/views/search.py
+++ frontend/views/search.py
@@ -9,20 +9,24 @@ def search():
     # Pole, ve kterých se prohledává se nastavují přímo v MongoDB
     # konkrétně v indexu s název 'text' v MongoDB
     # index 'text' musí být vytvořen alespoň s jedním polem, jinak tato stránka
-    # vrátí err
-    query = request.form['mainSearch']
-    results = mongo.db.harvest.find(
-        {
-            '$text': {'$search': query}
-        },
-        {
-            'paths.harvestID': True,
-            'harvest.harvestName': True,
-            'harvest.date': True,
-            'harvest.operator': True
-        }
-    )
+    # vrátí err, defaultní index:
+    # db.harvest.createIndex( { "harvest.harvestName": "text",
+    #                           "harvest.description": "text",
+    #                           "paths.harvestID": "text" } )
+    if request.method == 'POST':
+        query = request.form['query']
+        results = mongo.db.harvest.find(
+            {
+                '$text': {'$search': query}
+            },
+            {
+                'paths.harvestID': True,
+                'harvest.harvestName': True,
+                'harvest.date': True,
+                'harvest.operator': True
+            }
+        )
 
-    return render_template('search.html',
-                           results=results,
-                           query=query)
+        return render_template('search.html', results=results, query=query)
+    else:
+        return render_template('index.html')
